232927fc95909b7ddd91015c250e9e2bb7e767f9
Sitecreator <sigmasonic3012@gmail.com>
2026-05-17 20:45:29 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+
+client.once('ready', () => console.log(`Logged in as ${client.user.tag}`));
+client.on('messageCreate', (msg) => {
+  if (msg.content === '!ping') msg.reply('Pong!');
+});
+
+client.login(process.env.DISCORD_TOKEN);
